4ccc38f1313dad69ae0ca15d06271ac7ad8ee8ac
Dmitry Gordienko <d.gordienko@gmail.com>
2022-11-25 10:32:08 +0200

    added check containers status

--- main.py
+++ main.py
@@ -36,6 +36,34 @@ def docker_restart():
             client.containers.get(contname).restart()
             logger.info('Restart {}'.format(contname))
 
+@app.command()
+def docker_check():
+    syscont = os.getenv('SYSTEM_CONTAINERS')
+    appcont = os.getenv('APP_CONTAINERS')
+    syscont = json.loads(syscont)
+    appcont = json.loads(appcont)
+    client = docker.from_env()
+    containers: list = client.containers.list()
+    for item in containers:
+        contname = item.name
+        container = list(filter(lambda x: x in contname, syscont))
+        if (len(container) == 1):
+            cont = client.containers.get(contname)
+            _status = cont.status
+            _name = cont.name
+            _id = cont.short_id
+            logger.info('{} {} {}'.format(_id, _name, _status))
+    # restart application containers
+    for item in containers:
+        contname = item.name
+        container = list(filter(lambda x: x in contname, appcont))
+        if (len(container) == 1):
+            cont = client.containers.get(contname)
+            _status = cont.status
+            _name = cont.name
+            _id = cont.short_id
+            logger.info('{} {} {}'.format(_id, _name, _status))
+
 
 if __name__ == "__main__":
     app()
--- test_app.py
+++ test_app.py
@@ -3,32 +3,68 @@ import os
 
 import docker
 from dotenv import load_dotenv
+from loguru import logger
 
 load_dotenv()  # take environment variables from .env.
 
-def test_env():
+
+def test_workflow_full():
+    """
+    Test and debug workflow
+    """
     exception: Exception = None
     syscont = os.getenv('SYSTEM_CONTAINERS')
-    appcont = os.getenv('APP_CONTAINERS')    
+    appcont = os.getenv('APP_CONTAINERS')
     syscont = json.loads(syscont)
     appcont = json.loads(appcont)
     client = docker.from_env()
     containers: list = client.containers.list()
-    assert len(syscont) == 3
     try:
         # restart system containers
         for item in containers:
             contname = item.name
             container = list(filter(lambda x: x in contname, syscont))
-            if(len(container) == 1):
+            if (len(container) == 1):
                 client.containers.get(contname).restart()
-        # restart application containers       
+        # restart application containers
         for item in containers:
             contname = item.name
             container = list(filter(lambda x: x in contname, appcont))
-            if(len(container) == 1):
-                client.containers.get(contname).restart()  
+            if (len(container) == 1):
+                client.containers.get(contname).restart()
     except Exception as e:
         exception = e
     assert exception is None
 
+
+def test_workflow_check():
+    exception: Exception = None
+    try:
+        syscont = os.getenv('SYSTEM_CONTAINERS')
+        appcont = os.getenv('APP_CONTAINERS')
+        syscont = json.loads(syscont)
+        appcont = json.loads(appcont)
+        client = docker.from_env()
+        containers: list = client.containers.list()
+        for item in containers:
+            contname = item.name
+            container = list(filter(lambda x: x in contname, syscont))
+            if (len(container) == 1):
+                cont = client.containers.get(contname)
+                _status = cont.status
+                _name = cont.name
+                _id = cont.short_id
+                logger.info('{} {} {}'.format(_id, _name, _status))
+        # restart application containers
+        for item in containers:
+            contname = item.name
+            container = list(filter(lambda x: x in contname, appcont))
+            if (len(container) == 1):
+                cont = client.containers.get(contname)
+                _status = cont.status
+                _name = cont.name
+                _id = cont.short_id
+                logger.info('{} {} {}'.format(_id, _name, _status))            
+    except Exception as e:
+        exception: Exception = e
+    assert exception == None
